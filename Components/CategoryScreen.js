6cbe2fd64b3e01e511da58e88e0edd4cc86eb78e
jin0220 <37447282+jin0220@users.noreply.github.com>
2021-10-14 17:27:15 +0900

    카테고리 페이지
    카테고리 페이지, 제품 리스트 페이지,  상세 페이지 추가

--- Components/CategoryScreen.js
+++ Components/CategoryScreen.js
@@ -1,13 +1,41 @@
 import * as React from 'react';
-import { View, Text, Button, StyleSheet, TextInput, ScrollView } from 'react-native';
+import { View, Text, Button, StyleSheet, TextInput, ScrollView, TouchableOpacity, FlatList } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import ItemsListScreen from './ItemsListScreen';
+import ItemDetailScreen from './ItemDetailScreen';
+import { AntDesign } from '@expo/vector-icons';
 
-function CategoryScreen() {
+function CategoryScreen({ navigation }) {
     return (
         <SafeAreaView style={styles.container}>
-            <Text>Category Screen</Text>
-        </SafeAreaView >
+            {/*상태바 아래에 간격이 생기는 이유 - SafeAreaView 때문*/}
+            <FlatList
+                data={[
+                    { key: '리스트1' },
+                    { key: '리스트2' },
+                    { key: '리스트3' },
+                    { key: '리스트4' },
+                    { key: '리스트5' },
+                    { key: '리스트6' },
+                    { key: '리스트7' },
+                    { key: '리스트8' },
+                    { key: '리스트9' },
+                    { key: '리스트10' },
+                ]}
+                renderItem={({ item }) =>
+                    <View style={styles.categoryListBox}>
+                        <TouchableOpacity style={styles.categoryList} onPress={() => navigation.navigate('Items')}>
+                            <View style={styles.box}>
+                                <View style={styles.categoryicon} />
+                                <Text style={styles.categoryName}>{item.key}</Text>
+                            </View>
+                            <AntDesign name="right" size={18} color="#888" />
+                        </TouchableOpacity>
+                    </View>
+                }
+            />
+        </SafeAreaView>
     );
 }
 
@@ -15,8 +43,10 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator /*screenOptions={{ headerShown: false }}*/>
             <Stack.Screen name="Main" component={CategoryScreen} />
+            <Stack.Screen name="Items" component={ItemsListScreen} />
+            <Stack.Screen name="Detail" component={ItemDetailScreen} />
         </Stack.Navigator>
     );
 }
@@ -24,7 +54,37 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'flex-start',
-        // justifyContent: 'center',
+    },
+    categoryListBox: {
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingVertical: 10,
+        marginHorizontal: 15,
+        marginVertical: 5,
+        // borderBottomColor: '#ddd',
+        // borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 55,
+        backgroundColor: '#fff',
+    },
+    categoryList: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between', //요소들 사이에 동일한 간격을 둔다.
+        width: '100%',
+        height: 50,
+    },
+    box: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    categoryicon: {
+        width: 30,
+        height: 30,
+        marginRight: 10,
+        backgroundColor: '#ddd'
+    },
+    categoryName: {
+        fontSize: 17,
     },
 });
---
+++ Components/ItemsListScreen.js
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function ItemsListScreen({ navigation }) {
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={[
+                    { key: '리스트1' },
+                    { key: '리스트2' },
+                    { key: '리스트3' },
+                    { key: '리스트4' },
+                    { key: '리스트5' },
+                    { key: '리스트6' },
+                    { key: '리스트7' },
+                    { key: '리스트8' },
+                    { key: '리스트9' },
+                    { key: '리스트10' },
+                ]}
+                renderItem={({ item }) =>
+                    <View style={styles.ItemsListBox}>
+                        <TouchableOpacity style={styles.ItemsList} onPress={() => navigation.navigate('Detail')}>
+                            {/* <View style={styles.categoryicon}/> */}
+                            <View style={styles.imageBox}>
+                                {/* <Image style={styles.item}/> */}
+                            </View>
+                            <View style={styles.box}>
+                                <Text style={styles.itemManufacturing}>제조업체</Text>
+                                <Text style={styles.itemName}>{item.key}</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                }
+            />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // alignItems: 'flex-start',
+        // justifyContent: 'center',
+        backgroundColor: '#fff',
+    },
+    ItemsListBox: {
+        paddingLeft: 20,
+        paddingRight: 15,
+        paddingVertical: 10,
+        borderBottomColor: '#ddd',
+        borderBottomWidth: 1,
+        justifyContent: 'center',
+        height: 80,
+
+    },
+    ItemsList: {
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    imageBox: {
+        width: 60,
+        height: 60,
+        marginRight: 10,
+        borderWidth: 1,
+        borderRadius: 20,
+        borderColor: '#ddd',
+    },
+    box: {
+        marginLeft: 10,
+    },
+    itemManufacturing: {
+        color: '#888'
+    },
+    itemName: {
+        fontSize: 16
+    },
+});
