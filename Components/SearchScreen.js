c58584c0a3e4ee367f649f77f73930dc27c4a9a1
rgl-za <hyeonjeong2829@gmail.com>
2021-11-30 17:51:26 +0900

    검색 필터링 페이지 (수정 필요)

--- Components/SearchScreen.js
+++ Components/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { View, Text, Button, StyleSheet, TextInput, ScrollView, Image } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -7,100 +7,82 @@ import { FlatList, State, TouchableOpacity } from 'react-native-gesture-handler'
 import { SearchBar } from 'react-native-screens';
 import Detail from './ItemDetailScreen';
 
-export default function SearchScreen({navigation}) {
+export default function SearchScreen({navigation, route}) {
+    
     // const title = route.params.title;
 
-    //지금 가장 많이 검색하는? 
-    // const searchFilter = [' 최근에 검색한 제품'];
-
-    // const [searchIndex, setSearchIndex] = React.useState(0);
-    // const [search, setsearch] = useState('');
-
-
-    // const SearchList = () => {
-    //     return( 
-    //         <View style={styles.searchResult}>
-    //             {searchFilter.map((item, index) => (
-    //                 <TouchableOpacity 
-    //                     key={index} 
-    //                     activeOpacity={0.8}
-    //                     onPress={() => getItem(item)}>
-    //                     <Text 
-    //                         style={[
-    //                             styles.searchText, 
-    //                             searchIndex == index && styles.searchTextSelected,
-    //                             ]}>
-    //                             {item.id}
-    //                             {'.'}
-    //                             {item.title.toUpperCase()}
-    //                     </Text>
-    //                 </TouchableOpacity>
-    //             ))}
-    //         </View>
-    //     );
-    // };
-
-    // const getItem = (item) => {
-    //     alert('id'+item.id+'title'+item.title)
-    // };
-
-    // const [search, setSearch] = useState('');
-    // const [filteredDataSource, setFilteredDataSource] = useState([]);
-    // const [masterDataSource, setMasterDataSource] = useState([]);
-
-    // useEffect(() => {
-    //     getData();
-    // }, [page]);
+    const [searchVal, setSearchVal] = useState('');
+    const [data, setData] = useState('');
+    const [page, setPage] = useState(1);
+    const searchRef = useRef();
     
     const searchData = async(text) => {
-        // this.setState({loding:true});
-        
+        console.log(text);
+        // setSearchVal(text);
+
         const key = '6PsAAbQQMqw6BXq4X0X2Qv5nMMZgKAbGtiA1pBuujX1Cyic%2Bz3PN47Rir5uopLeWVy6AJxFT94YkJ%2BVE39XR3A%3D%3D';
 
         var url = 'http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService'; /*URL*/
         var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + key; /*Service Key*/
-
+        // queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent(text); 
         queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /**/
-        // queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(page); /*데이터 페이지*/
+        queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(page); /*데이터 페이지*/
         queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('20'); /*데이터 받아오는 개수*/
 
 
-    fetch(
-        url + queryParams,{
-            method: 'POST',
-            body: JSON.stringify({
-                "prdlstNm": prdlstNm
-            })
+        const response = await fetch(
+            url + queryParams,
+            {
+                method: 'GET',
+                // body: JSON.stringify({
+                //    prdlstNm
+                // }),
+            });
+
+        if (response.status === 200) {
+            setSearchVal(text);
+            const responseJson = await response.json();
+            const newresponseJson = responseJson.list;
+            setData(newresponseJson);
+            
+        } else {
+            console.log('fetch error33');
+            // throw new Error('unable to get');
         }
-    )
+    };
 
-      .then((response) => response.json())
-      .then((responseJson) => {
-        setData(responseJson.item);
+    useEffect(() => {}, [page]);
 
-        console.log(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    const renderItem = ({item}) => {
+      return (
+          
+        <View style={styles.container}>
+            <View style={styles.ItemsListBox}>
+                 <TouchableOpacity
+                       style={styles.ItemsList}
+                        onPress={() => navigation.navigate('Detail', { prdlstReportNo: item.prdlstReportNo })}>
+                <View style={styles.imageBox}>
+                    <Image style={{ width: 40, height: 40 }} source={{ uri: item.imgurl1 }} />
+                </View>
+                <View style={styles.box}>
+                    <Text style={styles.itemManufacturing}>{item.manufacture}</Text>
+                    <Text style={styles.itemName}>{item.prdlstNm}</Text>
+                </View>
+                </TouchableOpacity>
+            </View>  
+        </View>
+       
+      );
+      
   };
-
-
-//   const renderItem = ({item}) => {
   
-    
-// }
+    const clearInput = () => {
+        searchRef.current.ref.clear();
+        setSearchVal("");
+        setData("");
 
-//     const renderItem = ({item}) => {
-//       return (
-//         <ResultList
-//         style={styles.ItemsList}
-//         onPress={() => navigation.navigate('Detail', { prdlstReportNo: item.prdlstReportNo })}>
-//             <ResultItem>{item.prdlstNm}</ResultItem>
-//         </ResultList>
-//       )
-//   }
-  
+    };
+
 
     return (
         <View style={styles.container}>
@@ -108,70 +90,32 @@ export default function SearchScreen({navigation}) {
                 <View>
                     <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 100}}>궁금한 제품을 검색해보세요!</Text>
                 </View>
-                {/* <FontAwesome5 name="heart" size={24} color='#D9B650'/> */}
+                {/* 버튼 <FontAwesome5 name="heart" size={24} color='#D9B650'/> */}
             </View>
             <View style={{marginTop: 30, flexDirection: 'row'}}>
                 <View style={styles.searchContainer}>
                     <Ionicons name="search-outline" size={25} style={{marginLeft: 20}} />
-                    <TextInput placeholder="상품 이름을 입력해주세요" style={styles.input} onChangeText ={text => {searchData(text)}}/>
+                    <TextInput ref={searchRef} placeholder="상품 이름을 입력해주세요" style={styles.input} onChangeText ={text => {searchData(text)} }/>
                 </View>
+                
+                {/* 버튼
                 <View style={styles.sortBtn}>
                     <MaterialIcons name="sort" size={30} color={'white'}/>
-                </View>
+                </View> */}
             </View>
             {/* <SearchList/> */}
             
-            {/* <FlatList
-            
-                data={filterData}
-                keyExtractor={(item, index) => 'key' + index}
-                onEndReached={() => setPage(page+1)}
-                renderItem={({ item }) =>
-                    <View style={styles.ItemsListBox}>
-                        <TouchableOpacity
-                            style={styles.ItemsList}
-                            onPress={() => navigation.navigate('Detail', { prdlstReportNo: item.prdlstReportNo })}
-                        >
-                            <View style={styles.imageBox}>
-                                <Image style={{ width: 40, height: 40 }} source={{ uri: item.imgurl1 }} />
-                            </View>
-                            <View style={styles.box}>
-                                <Text style={styles.itemManufacturing}>{item.manufacture}</Text>
-                                <Text style={styles.itemName}>{item.prdlstNm}</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                }
-            /> */}
             <FlatList
-                // data={getData}
-                keyExtractor={(item, index) => 'key' + index}
+                data={data}
+                renderItem={ renderItem } 
+                keyExtractor={(item, index) => index.toString()}
                 // onEndReached={() => setPage(page + 1)}
-                renderItem={({ item }) =>
-                    <View style={styles.ItemsListBox}>
-                        <TouchableOpacity
-                            style={styles.ItemsList}
-                            onPress={() => navigation.navigate('Detail', { prdlstReportNo: item.prdlstReportNo })}
-                        >
-                            <View style={styles.imageBox}>
-                                <Image style={{ width: 40, height: 40 }} source={{ uri: item.imgurl1 }} />
-                            </View>
-                            <View style={styles.box}>
-                                <Text style={styles.itemManufacturing}>{item.manufacture}</Text>
-                                <Text style={styles.itemName}>{item.prdlstNm}</Text>
-                            </View>
-                        </TouchableOpacity>
-                    </View>
-                    // <ResultList
-                    //     style={styles.ItemsList}
-                    //     onPress={() => navigation.navigate('Detail', { prdlstReportNo: item.prdlstReportNo })}>
-                    //     <ResultItem>{item.prdlstNm}</ResultItem>
-                    // </ResultList>
-                }
+                
             />
         </View >
     );
 }
+
 
 
 const styles = StyleSheet.create({
@@ -265,7 +209,7 @@ const styles = StyleSheet.create({
 });
 
 
-
+////////////////////////////////////////////////////////////
 
 // import React, { useState, useEffect } from 'react';
 
@@ -284,35 +228,49 @@ const styles = StyleSheet.create({
 // const [filteredDataSource, setFilteredDataSource] = useState([]);
 // const [masterDataSource, setMasterDataSource] = useState([]);
 
-// useEffect(() => {
+// const [searchVal, setSearchVal] = useState('');
 
-//     const key = '6PsAAbQQMqw6BXq4X0X2Qv5nMMZgKAbGtiA1pBuujX1Cyic%2Bz3PN47Rir5uopLeWVy6AJxFT94YkJ%2BVE39XR3A%3D%3D';
+
+// useEffect(() => {
+//     fetch();
+// }, []);
+
+
+
+// const fetch = async (text) => {
+
+//         setSearchVal(text);
+
+
+//         const key = '6PsAAbQQMqw6BXq4X0X2Qv5nMMZgKAbGtiA1pBuujX1Cyic%2Bz3PN47Rir5uopLeWVy6AJxFT94YkJ%2BVE39XR3A%3D%3D';
 
 //         var url = 'http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService'; /*URL*/
 //         var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + key; /*Service Key*/
-
+//         queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent(text);
 //         queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /**/
 //         // queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(page); /*데이터 페이지*/
 //         queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('20'); /*데이터 받아오는 개수*/
 
 
-//     fetch(
+//         const response = await fetch(
 //         url + queryParams,{
 //             method: 'GET',
 //         }
-//     )
+//         );
 
-//       .then((response) => response.json())
-//       .then((responseJson) => {
-//         setFilteredDataSource(responseJson);
-//         setMasterDataSource(responseJson);
+//         if(response.status === 200){
+//             const responseJson = await response.json(); 
+//             setFilteredDataSource(responseJson);
+//             setMasterDataSource(responseJson);
+        
+//         }
 
-//         // console.log(responseJson);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//       });
-//   }, []);
+//       else{
+//           console.log('error');
+//       }
+//   };
+  
+
 
 //   const searchFilterFunction = (text) => {
 //     // Check if searched text is not blank
@@ -406,3 +364,166 @@ const styles = StyleSheet.create({
 //     backgroundColor: '#FFFFFF',
 //   },
 // });
+
+//////////////////////////////////////////////////////////
+
+/*
+import React, { useState, useRef, useEffect } from "react";
+import { FlatList, Text } from "react-native";
+import * as Animatable from "react-native-animatable";
+import styled from "styled-components";
+import { search } from "../../config";
+import mixIn from "../../Styles/Mixin";
+
+export default function Search({ navigation }) {
+  const [searchVal, setSearchVal] = useState("");
+  const [data, setData] = useState();
+  const searchRef = useRef();
+
+  const searchData = async (text) => {
+    try {
+      setSearchVal(text);
+      const res = await fetch(`${search}`, {
+        method: "POST",
+        body: JSON.stringify({
+          keyword: text,
+        }),
+      });
+      const resJson = await res.json();
+      const newResJson = resJson.products;
+      setData(newResJson);
+    } catch (e) {
+      console.log("페치에 실패했습니다.");
+    }
+  };
+
+  useEffect(() => {}, []);
+
+  const renderItem = ({ item }) => {
+    return (
+      <ResultList
+        onPress={() =>
+          navigation.navigate("ProductDetail", {
+            productId: item.id,
+          })
+        }
+      >
+        <ResultItem>{item.name}</ResultItem>
+      </ResultList>
+    );
+  };
+
+  const clearInput = () => {
+    searchRef.current.ref.clear();
+    setSearchVal("");
+    setData("");
+  };
+
+  return (
+    <Container>
+      <SearchBarWrap>
+        <SearchIcon
+          source={{
+            uri: "https://webstockreview.net/images/search-icon-png-4.png",
+          }}
+          touch={searchVal}
+        />
+        <SearchBar
+          ref={searchRef}
+          placeholder="     검색어를 입력해 주세요"
+          onChangeText={(text) => searchData(text)}
+          touch={searchVal}
+          animation={searchVal.length > 0 ? typed : false}
+        />
+        <Cancel
+          touch={searchVal}
+          animation={searchVal.length > 0 ? btnIn : false}
+          onPress={() => clearInput()}
+        >
+          <Text>취소</Text>
+        </Cancel>
+      </SearchBarWrap>
+      <ResultContainer>
+        <ResultLabel>상품 바로가기</ResultLabel>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          keyExtractor={(item, idx) => idx.toString()}
+        />
+      </ResultContainer>
+    </Container>
+  );
+}
+
+const Container = styled.View``;
+
+const SearchBarWrap = styled.View`
+  position: relative;
+  ${mixIn.flex("row", "flex-start", "center")};
+  width: 100%;
+`;
+
+const Cancel = Animatable.createAnimatableComponent(styled.TouchableOpacity`
+  display: ${({ touch }) => (touch.length > 0 ? "flex" : "none")};
+  padding-left: 10px;
+`);
+
+const SearchIcon = styled.Image`
+  position: absolute;
+  left: 19;
+  width: 20px;
+  height: 20px;
+  z-index: ${({ touch }) => (touch.length > 0 ? -1 : 1)};
+`;
+
+const SearchBar = Animatable.createAnimatableComponent(styled.TextInput`
+  width: ${({ touch }) => (touch.length > 0 ? "80%" : "95%")};
+  height: 50px;
+  margin: 10px;
+  padding-left: 10px;
+  border-radius: 10;
+  background-color: ${({ theme }) => theme.color.White};
+`);
+
+const ResultContainer = styled.View`
+  height: 100%;
+  background-color: ${({ theme }) => theme.color.White};
+`;
+
+const ResultLabel = styled.Text`
+  height: 50px;
+  padding-left: 10px;
+  line-height: 60px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.color.SubtitlePaleGrey};
+`;
+
+const ResultList = styled.TouchableOpacity`
+  height: 60px;
+  padding-left: 10px;
+  border: 0.3px solid #ddd;
+  background-color: ${({ theme }) => theme.color.White};
+`;
+
+const ResultItem = styled.Text`
+  line-height: 60px;
+`;
+
+const typed = {
+  0: {
+    width: "95%",
+  },
+  1: {
+    width: "80%",
+  },
+};
+
+const btnIn = {
+  0: {
+    animation: false,
+  },
+  1: {
+    animation: "slideInRight",
+  },
+};
+*/
