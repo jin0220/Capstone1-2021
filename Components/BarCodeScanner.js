0278e969f48b97ef69d92cf5bc7100712d51439f
rgl-za <hyeonjeong2829@gmail.com>
2021-12-10 11:53:58 +0900

    보고서 제출용 주석 추가

--- Components/BarCodeScanner.js
+++ Components/BarCodeScanner.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Button, Dimensions } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import { FontAwesome5, Ionicons, Entypo, Feather, MaterialIcons } from '@expo/vector-icons';
-// import Constants from 'expo-constants';
-import NoSearch from './NoSearch';
-import ItemDetailScreen from './ItemDetailScreen';
 
 const { width } = Dimensions.get('window')
 
@@ -16,20 +12,18 @@ export default function App({navigation}) {
   var Flag1;
   var Flag2;
   
-
+// 바코드 스캔
   useEffect(() => {
     (async () => {
-      const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const { status } = await BarCodeScanner.requestPermissionsAsync(); // 사용자에게 카메라 액세스 권한을 부여하도록 요청
+      setHasPermission(status === 'granted'); 
     })();
     setScanned(false);
   }, []);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
-    // alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-    getRepotNo(data);
-    // getRawmt(data);
+    getRepotNo(data); // 스켄힌 바코드 정보를 바코드 연계 API 요청인자로 전달
   };
 
   if (hasPermission === null) {
@@ -39,60 +33,41 @@ export default function App({navigation}) {
     return <Text>카메라에 접근할 수가 없습니다</Text>;
   }
 
-
-  const getRepotNo = async (data) => {
-    // console.log(data);
+// 바코드 연계 API
+  const getRepotNo = async (data) => { 
     const response = await fetch(
       'http://openapi.foodsafetykorea.go.kr/api/' + '3e9c040903bd4eec95e1' + '/C005/json/1/5/BAR_CD='
-
-      // 'http://openapi.foodsafetykorea.go.kr/api/' + '3e9c040903bd4eec95e1' + '/C005/json/1/5/BRCD_NO='
-
         +
       data,
       {
         method: 'GET',
       },
     );
-    if(response.status === 200){
+    if(response.status === 200){ // API 연결 성공시
       const responseJson = await response.json();
-      // PrdlstNum = responseJson.C005.row[0]['PRDLST_REPORT_NO'];
-      Flag1 = responseJson.C005['total_count'];
-      // Code = responseJson.C005.RESULT['CODE'];
-      console.log('===barcode===');
-      // console.log(responseJson);
-      // console.log(Code);
-
-      // PrdlsName = responseJson//.C005.row[0]['PRDLST_NM'];
-      // console.log(responseJson.C005.row[0]);
-      // console.log(responseJson.C005['total_count']);
-      if(Flag1 !== '0'){
-        PrdlstNum = responseJson.C005.row[0]['PRDLST_REPORT_NO'];
-        getRawmt(PrdlstNum);
-      } else{
-        console.log('missing');
-        navigation.navigate('NoSearch');}
-        // return responseJson.C005.row[0];
-        // console.log(responseJson.C005.row[0]['PRDLST_REPORT_NO']);
+      Flag1 = responseJson.C005['total_count']; // 
+     
+      if(Flag1 !== '0'){ // 스캔한 바코드가 바코드 연계 API에 데이터가 있으면
+        PrdlstNum = responseJson.C005.row[0]['PRDLST_REPORT_NO']; 
+        getRawmt(PrdlstNum); // HACCAP 제품이미지 및 포장지표기정보 API 요청인자로 품목보고번호를 넘겨줌
+      } else{ // 스캔한 바코드가 바코드 연계 API에 데이터가 없으면
+        navigation.navigate('NoSearch'); // 해당 제품을 찾을 수 없음 페이지로 이동
       } 
-      else {
-      return 0;
+    } else { // API 연결 실패시
+      return 0; // 종료
     }
   };
 
-
+// HACCAP 제품이미지 및 포장지표기정보 API 
   const getRawmt = async (reportnum) => {
     const key = '6PsAAbQQMqw6BXq4X0X2Qv5nMMZgKAbGtiA1pBuujX1Cyic%2Bz3PN47Rir5uopLeWVy6AJxFT94YkJ%2BVE39XR3A%3D%3D';
 
-    var url = 'http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService'; //URL
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + key; //Service Key
+    var url = 'http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService'; // URL
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + key; // Service Key
 
-    queryParams += '&' + encodeURIComponent('prdlstReportNo') + '=' + encodeURIComponent(PrdlstNum); 
-    // queryParams += '&' + encodeURIComponent('prdlstNm') + '=' + encodeURIComponent(PrdlstName); 
+    queryParams += '&' + encodeURIComponent('prdlstReportNo') + '=' + encodeURIComponent(PrdlstNum); // 바코드 연계 API에서 받은 요청인자(품목보고번호)
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); 
-    // queryParams += '&' + encodeURIComponent('pageNo'); 
-    // queryParams += '&' + encodeURIComponent('numOfRows'); 
-      // console.log(PrdlstName);
-    console.log(PrdlstNum);
+   
     const response = await fetch(
       url + queryParams,
       {
@@ -100,60 +75,15 @@ export default function App({navigation}) {
       },
     );
 
-    if (response.status === 200) {
-      // console.log('===haccp===');
+    if (response.status === 200) { // API 연결 성공시
       const responseJson = await response.json();
-      // console.log("check!!!!!!!!")
-      // console.log(responseJson['totalCount']);
       Flag2 = responseJson['totalCount'];
-      
-      // console.log('==check1==');
-      console.log('===haccp===');
 
-      if(Flag2 === '0'){
-        console.log('missing');
-        navigation.navigate('NoSearch');
+      if(Flag2 === '0'){ // (바코드 연계 API로부터 받은) 제품품목보고번호가 HACCAP API에 없으면
+        navigation.navigate('NoSearch'); // 해당 제품을 찾을 수 없음 페이지로 이동
 
-      }else{
-      // console.log(PrdlstName);
-      PrdlstName = responseJson.list[0]['prdlstNm'];
-      getIngredient(PrdlstName);
-
-
-      
-      const raw_mt = responseJson.list[0]['rawmtrl'];
-      var mt = raw_mt.split(/[\,\(\)\%]/);
-
-      var mta = [];
-
-      for(var i =0; i<mt.length; i++){
-        mta.push(mt[i]);
-      }
-      mta = mta.filter(item => item);
-      console.log(mta);
-
-
-
-
-      const nut_ri = responseJson.list[0]['nutrient'];
-      var ri = nut_ri.split(/[\(\)\,]/);
-
-      var ent =[];
-
-      for(var i=0; i<ri.length; i++){
-        ent.push(ri[i]);
-      }
-
-      ent = ent.filter(item => item);
-      console.log(ent);
-
-      
-
-
-
-
-      // return responseJson.C002.row[0].RAWMTRL_NM;
-
+      }else{ // (바코드 연계 API로부터 받은) 제품 품목보고번호가 HACCAP API에 있으면
+      // 해당 제품 품목보고 번호를 제품 상세 페이지로 넘겨줌
       navigation.navigate('ItemDetailScreen', {prdlstReportNo: PrdlstNum, name: PrdlstName});
       
     }
@@ -163,70 +93,26 @@ export default function App({navigation}) {
       // throw new Error('unable to get');
     }
   };
-
-
-const getIngredient = async(reportnum) => {
-  const key = '6PsAAbQQMqw6BXq4X0X2Qv5nMMZgKAbGtiA1pBuujX1Cyic%2Bz3PN47Rir5uopLeWVy6AJxFT94YkJ%2BVE39XR3A%3D%3D';
-
-  var url = 'http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1';
-  var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + key; /* Service Key*/
-  queryParams += '&' + encodeURIComponent('desc_kor') + '=' + encodeURIComponent(PrdlstName); /* */
-  // queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
-  // queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('3'); /* */
-  // queryParams += '&' + encodeURIComponent('bgn_year') + '=' + encodeURIComponent('2017'); /* */
-  // queryParams += '&' + encodeURIComponent('animal_plant') + '=' + encodeURIComponent('(유)돌코리아'); /* */
-  queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json'); /* */
-
-  console.log(PrdlstName);
-  
-    const response = await fetch(
-      url + queryParams,
-      {
-        method: 'GET',
-      },
-    );
-
-    if (response.status === 200) {
-      console.log('==ingredient==');
-      const responseJson = await response.json();
-
-      if(PrdlstName === responseJson.body.items[0]['DESC_KOR']){
-        console.log(responseJson);
-        console.log('sucess');
-      }
-
-      // console.log(responseJson);
-      // return responseJson.C002.row[0].RAWMTRL_NM;
-    } else {
-      return 0;
-      // throw new Error('unable to get');
-    } 
-  };
   
   return (
       <BarCodeScanner
-        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} // 바코드 스캔이 성공했을 때 콜백되는 함수, 스캔 결과를 받아 실행
         style={[StyleSheet.absoluteFillObject, styles.container]}>
-        
-        {/* <Text style={ styles.text}>바코드를 스캔해주세요</Text> */}
-        {/* <View style={styles.cancel}> */}
        
         <View style={styles.layerTop} />
-        <Text style={styles.text}>바코드를 스캔해주세요</Text>
-        <View style={styles.layerCenter}>
-        <View style={styles.layerLeft} />
-        <View style={styles.focused} />
-        <View style={styles.layerRight} />
-        {/* {scanned && <Button title={'다시 스캔하려면 누르세요'} onPress={() => setScanned(false)}/>} */}
-        {/* </View> */}
+          <Text style={styles.text}>바코드를 스캔해주세요</Text>
+          <View style={styles.layerCenter}>
+          <View style={styles.layerLeft} />
+          <View style={styles.focused} />
+          <View style={styles.layerRight} />
         </View>
-        {/* {scanned && <Button title={'다시 스캔하려면 누르세요'} onPress={() => setScanned(false)}/>} */}
         <View style={styles.layerBottom} />
         {scanned && <Button title={'다시 스캔하려면 누르세요'} onPress={() => setScanned(false)}/>}
     </BarCodeScanner>
   );
 }
 
+// 해당 페이지 CSS
 const opacity = 'rgba(0, 0, 0, .6)';
 const styles = StyleSheet.create({
   container: {
